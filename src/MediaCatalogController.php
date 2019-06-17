3cda884af990a4e8aa99f3a8422f2b163fb329af
Isaac Daniel Reyna <email@isaacreyna.com>
2019-06-16 22:48:39 -0500

    Added show allowed action
    Enables to view specific media instance.

--- src/MediaCatalogController.php
+++ src/MediaCatalogController.php
@@ -3,12 +3,26 @@
 namespace IsaacDanielReyna\MediaCatalog;
 
 use PageController;
+use SilverStripe\Control\HTTPRequest;
 
 class MediaCatalogController extends PageController
 {
-    public function Catalog()
+    private static $allowed_actions = [
+        'show'
+    ];
+
+    public function show(HTTPRequest $request)
     {
-        return Media::get();
+        $media = Media::get()->byID($request->param('ID'));
+
+        if (!$media){
+            return $this->httpError(404,'Sorry, it seems you were trying to access a page that doesn\'t exist.');
+        }
+
+        return [
+            'Media' => $media,
+            'Title' => $media->Title
+        ];
     }
 
 }
---
+++ templates/IsaacDanielReyna/MediaCatalog/Layout/MediaCatalog_show.ss
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="row">
+        <div class="col">
+            <% with $Media %>
+            $Image
+            <p><strong>$NativeTitle</strong> $Transliteration - $Description</p>
+            
+            <% end_with %>
+        </div>
+    </div>
+</div>
